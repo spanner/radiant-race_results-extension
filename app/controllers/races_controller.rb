a82b99256c8b7af5a4e06840030b2574dbdb55f7
spanner <will@grytpype-3.home>
2010-04-14 07:51:01 +0100

    no_login_required

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,5 +1,6 @@
 class RacesController < ApplicationController
   radiant_layout { |controller| controller.layout_for :races }
+  no_login_required
 
   def show
     @race = Race.find_by_slug(params[:slug])
